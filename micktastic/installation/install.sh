8ef06f6c7ff4920914c7d23cd1f8a4ab850c4bf1
MickeyKay <mickeyskay@gmail.com>
2015-11-05 12:02:24 -0800

    Updated something and now it works - wtf.

--- micktastic/installation/install.sh
+++ micktastic/installation/install.sh
@@ -7,12 +7,6 @@
 VAGRANT_DIR=~/vagrant-local
 
 #
-#  Install Homebrew
-#
-ruby -e “$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)”
-
-
-#
 # Homebrew install items
 #
 echo 'Updating Homebrew.'
