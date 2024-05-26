6a5cc354efb9fab259df77089689dc91dee69609
MickeyKay <mickeyskay@gmail.com>
2024-05-25 21:53:19 -0700

    Remove faulty paths/aliases

--- system/_path.zsh
+++ system/_path.zsh
@@ -1,3 +1,2 @@
 export PATH="./bin:/usr/local/bin:/usr/local/sbin:$ZSH/bin:/usr/local/mysql/bin:$PATH"
 export MANPATH="/usr/local/man:/usr/local/mysql/man:/usr/local/git/man:$MANPATH"
-export PATH="$(brew --prefix homebrew/php/php71)/bin:$PATH"
