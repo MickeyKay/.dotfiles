8bad4c7438a812b1307b9bf2e5492f60bb9bc297
MickeyKay <mickeyskay@gmail.com>
2024-05-25 21:56:01 -0700

    Remove unused aliases

--- micktastic/directories/aliases.zsh
+++ micktastic/directories/aliases.zsh
@@ -11,15 +11,9 @@ alias .....="cd ../../../.."
 # DOT files
 alias dotd="cd ~/.dotfiles"
 
-# GRUNT files
-alias gruntd="cd ~/.grunt-init"
-
 # Base directories
 alias root="cd /"
 alias home="cd ~/"
 alias desk="cd ~/Desktop"
 alias sites="cd ~/Sites"
 alias subUser="cd ~/Library/Application\ Support/Sublime\ Text\ 3/Packages/User"
-
-# Vagrant
-alias vn="vagrant"
